95d97bc03c404759833865eb3ba3c300e1fa3269
MychealL <mycheallepur@gmail.com>
2017-03-14 12:52:53 -0700

    Modified tunecast.py and CSS for heading/footer styles

--- tunecast.py
+++ tunecast.py
@@ -2,17 +2,20 @@ from flask import Flask
 from flask import url_for
 from flask import render_template
 
+
 app = Flask(__name__)
 
-
-
 @app.route("/")
-def hello():
-   	return render_template('home.html', title='home')
+def home():
+	return render_template('home.html', title='home')
 
-@app.route("/something")
-def something():
-	return "Something else!"
+@app.route("/list")
+def list():
+	return render_template('list.html',title='list')
+
+@app.route("/settings")
+def settings():
+	return render_template('settings.html',title='settings')
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
