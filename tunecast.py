b58b0a08244e2273a1926ef5dd6a4c33064c4c27
MychealL <mycheallepur@gmail.com>
2017-03-14 15:33:29 -0700

    Modified tunecast.py
    added new imports such as request, print_function, and OS
    added new variables such as music_dir, music_files and music_files_number
    changed the app to run with debug=True

--- tunecast.py
+++ tunecast.py
@@ -1,21 +1,31 @@
-from flask import Flask
-from flask import url_for
-from flask import render_template
+from __future__ import print_function
+from flask import Flask, request, render_template, url_for
+import os
 
+music_dir = 'static/music'
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-	return render_template('home.html', title='home')
+    return render_template('home.html', title='home')
+
 
 @app.route("/list")
 def list():
-	return render_template('list.html',title='list')
+    music_files = [f for f in os.listdir(music_dir) if f.endswith('mp3')]
+    music_files_number = len(music_files)
+    return render_template('list.html',
+                           title='list',
+                           music_files_number=music_files_number,
+                           music_files=music_files)
+
 
 @app.route("/settings")
 def settings():
-	return render_template('settings.html',title='settings')
+    return render_template('settings.html', title='settings')
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run(debug=True)
