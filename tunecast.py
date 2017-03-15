8028cbc57f49f0b60505d44c2441f0dd0387567f
MychealL <mycheallepur@gmail.com>
2017-03-14 18:50:39 -0700

    Modified tunecast.py
    Made music_files and music_files_number global variables
    Added threaded=True to app.run in order to fix Errno 10053

--- tunecast.py
+++ tunecast.py
@@ -3,23 +3,27 @@ from flask import Flask, request, render_template, url_for
 import os
 
 music_dir = 'static/music'
+music_files = [f for f in os.listdir(music_dir) if f.endswith('mp3')]
+music_files_number = len(music_files)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('home.html', title='home')
+    return render_template('home.html',
+                           title='home',
+                           music_files=music_files,
+                           music_files_number=music_files_number)
 
 
 @app.route("/list")
 def list():
-    music_files = [f for f in os.listdir(music_dir) if f.endswith('mp3')]
-    music_files_number = len(music_files)
     return render_template('list.html',
                            title='list',
                            music_files_number=music_files_number,
                            music_files=music_files)
+
 
 
 @app.route("/settings")
@@ -28,4 +32,4 @@ def settings():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
